f3b27e281e79841dbac0ea01bad50819b0f61b85
aggotbra <v2agolcov@gmail.com>
2025-09-08 05:10:24 +0300

    chore: touch ping.js to ensure deploy picks it

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
   console.log("[ping] hit", { method: req.method, url: req.url });
-  res.status(200).json({ ok: true });
+  res.status(200).json({ ok: true, route: "/api/ping" });
 }
