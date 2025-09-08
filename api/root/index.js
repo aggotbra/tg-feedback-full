2b983b82af8bbc2b34cbc52d7e384bc3b6f00407
aggotbra <v2agolcov@gmail.com>
2025-09-08 05:42:42 +0300

    fix: move /api/root to folder and map / -> /api/root

---
+++ api/root/index.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader("content-type", "text/html; charset=utf-8");
+  res.status(200).send(
+    "<!doctype html><html><head><meta charset='utf-8'><title>tg-feedback · status</title></head>" +
+    "<body style='font-family:system-ui;padding:24px'>" +
+    "<h1>tg-feedback · status</h1>" +
+    "<ul>" +
+    "<li><a href='/api/admin'>/api/admin</a></li>" +
+    "<li><a href='/api/ping'>/api/ping</a></li>" +
+    "<li><a href='/api/telegram'>/api/telegram</a></li>" +
+    "</ul>" +
+    "</body></html>"
+  );
+}
